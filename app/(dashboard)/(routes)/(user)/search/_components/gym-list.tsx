6356d3ba4f26aca1f44c4b60090e26c0138781ce
Alex Sneshko <007alex03@gmail.com>
2024-05-25 01:17:38 +0300

    fix: gym list

--- app/(dashboard)/(routes)/(user)/search/_components/gym-list.tsx
+++ app/(dashboard)/(routes)/(user)/search/_components/gym-list.tsx
@@ -6,31 +6,33 @@ import { isUserSubcribed } from "@/lib/utils";
 import { auth } from "@clerk/nextjs";
 
 interface GymListProps {
-    data: GymWithSubscribers[]
+  data: GymWithSubscribers[];
 }
 
-export const GymList = ({
-    data
-}: GymListProps) => {
+export const GymList = ({ data }: GymListProps) => {
   const { userId } = auth();
 
   if (!userId) {
-    return null
+    return null;
   }
 
   if (data.length === 0) {
     return (
       <div className="w-hull mt-10 flex items-center justify-center">
-        <h1>No Users</h1>
+        <h1>No Gyms</h1>
       </div>
     );
   }
 
   return (
     <div>
-        {data.map((gym) => (
-            <GymCard key={gym.id} data={gym} isUserSubscribed={isUserSubcribed(gym, userId)}/>
-        ))}
+      {data.map((gym) => (
+        <GymCard
+          key={gym.id}
+          data={gym}
+          isUserSubscribed={isUserSubcribed(gym, userId)}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
